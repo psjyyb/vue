0251eb84106366866ad7c1d2db967f19a400e2ea
ass6728 <ass6728@naver.com>
2024-06-18 22:54:49 +0900

    commit

--- test/frontend/src/App.vue
+++ test/frontend/src/App.vue
@@ -15,8 +15,16 @@
     </div>
   </nav>
   <router-view/>
+  <Footer></Footer>
 </template>
-
+<script>
+import Footer from './components/Footer.vue'
+export default{
+        components: {
+    Footer
+     }
+    }
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- test/frontend/src/views/HomeView.vue
+++ test/frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
     <div><h3>*본 게시판은 따뜻한 소통과 자유롭게 정보 공유를 위해 운영되는 페이지 입니다.</h3></div>
     <div><h3>*욕설, 비방, 광고 등 타인에게 불쾌감을 줄 수 있는 내용은 삼가주시기 바랍니다.</h3></div>
   </div>
-  <Footer></Footer>
+ 
 </template>
 
 <script>
@@ -13,8 +13,5 @@ import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 export default {
   name: 'HomeView',
-  components: {
-    Header,Footer
-  }
 }
 </script>
